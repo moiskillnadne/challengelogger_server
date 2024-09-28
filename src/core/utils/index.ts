5c139bd03ba153a6530444c679164094339e80ec
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-09-28 12:49:09 +0200

    Added JWT service and auth middleware

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './generateOTP';
+export * from './jwt';
---
+++ src/core/utils/jwt.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+
+class JWTService {
+  private secret: string;
+
+  constructor() {
+    if (!process.env.JWT_SECRET_KEY) {
+      throw new Error('The JWT_SECRET_KEY variable not provided');
+    }
+
+    this.secret = process.env.JWT_SECRET_KEY;
+  }
+
+  public generateToken(payload: Record<string, unknown>): string {
+    return jwt.sign(payload, this.secret, {
+      expiresIn: 86400000, // 1 day
+    });
+  }
+
+  public verifyToken(token: string) {
+    return jwt.verify(token, this.secret);
+  }
+}
+
+export default new JWTService();

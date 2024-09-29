b1c1c60ddd5861ad9e6bb25a3681d2eb607e6c36
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-09-29 15:08:15 +0200

    Update build script

--- build.js
+++ build.js
@@ -4,6 +4,9 @@ const { dependencies } = require('./package.json');
 const env = {
   'process.env.ENV': JSON.stringify(process.env.ENV),
   'process.env.PORT': JSON.stringify(process.env.PORT),
+  'process.env.SQLITE_DB_PATH': JSON.stringify(process.env.SQLITE_DB_PATH),
+  'process.env.SENDGRID_API_KEY': JSON.stringify(process.env.SENDGRID_API_KEY),
+  'process.env.JWT_SECRET_KEY': JSON.stringify(process.env.JWT_SECRET_KEY)
 };
 
 esbuild

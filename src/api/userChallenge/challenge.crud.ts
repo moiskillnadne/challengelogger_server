8c61520bba0e5caefcceb2dd84f549ea4016f1f6
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-03 12:07:55 +0200

    Added endpoint for fetching list of challenges

--- src/api/userChallenge/challenge.crud.ts
+++ src/api/userChallenge/challenge.crud.ts
@@ -3,7 +3,15 @@ import { CreateChallengeDBPayload, FindByParams } from './validation.schema';
 import { UserChallenge } from '~/database/models/UserChallenge';
 
 export class UserChallengeCrud {
-  static findByParams(params: FindByParams) {
+  static findManyByUserId(userId: string) {
+    return UserChallenge.findAll({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  static findOneByParams(params: FindByParams) {
     return UserChallenge.findOne({
       where: {
         id: params.id,

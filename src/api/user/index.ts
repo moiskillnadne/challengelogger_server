74269b84cd4341cb1712b17e52f7e5ee6b9fd50b
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-04 19:14:44 +0200

    Change base errors on custom ones in /protected/user and auth middleware

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,13 +1,19 @@
 import express, { Request, Response } from 'express';
 
-import { authMiddleware, AuthorizedRequest } from '~/core/middleware/auth';
+import { ErrorMessages } from '~/core/dictionary/error.messages';
+import { UnauthorizedError } from '~/core/errors/UnauthorizedError';
+import { authMiddleWare } from '~/core/middleware/authorization';
 
 const route = express.Router();
 
-route.use(authMiddleware);
+route.use(authMiddleWare);
 
 route.get('/', (req: Request, res: Response) => {
-  const user = (req as AuthorizedRequest).user;
+  const user = req.user;
+
+  if (!user) {
+    throw new UnauthorizedError(ErrorMessages.unauthorized);
+  }
 
   return res.status(200).json({
     type: 'USER_FETCHED',
---
+++ src/api/user/interfaces.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  email: string;
+}

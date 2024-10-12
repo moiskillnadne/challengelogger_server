8a3efe91d74dd1b6673665a5e88bfc16eb3ed72b
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-12 22:22:42 +0200

    Check one more option

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,24 +1,16 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
-import { ErrorMessages } from '~/core/dictionary/error.messages';
-import { UnauthorizedError } from '~/core/errors';
+// import { ErrorMessages } from '~/core/dictionary/error.messages';
+// import { UnauthorizedError } from '~/core/errors';
 import { logger } from '~/core/logger';
-import { isAuthenticated } from '~/shared/user';
+// import { isAuthenticated } from '~/shared/user';
 
 const route = express.Router();
 
-route.get('/', (req: Request, res: Response, next: NextFunction) => {
+route.get('/', (req: Request, res: Response) => {
   const user = req.user;
 
   logger.info(`[GET /protected/user] User: ${JSON.stringify(user)}`);
-
-  if (!isAuthenticated(user)) {
-    return next(
-      new UnauthorizedError(
-        `[GET /protected/user] ${ErrorMessages.unauthorized}`,
-      ),
-    );
-  }
 
   return res.status(200).json({
     type: 'USER_FETCHED',

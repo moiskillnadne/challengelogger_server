6f4891ba467fe5cc49ba132879bdd52afdf77704
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-14 00:39:29 +0200

    Updated base endpoint path

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import '~/integration/Sentry';
 
 import * as Sentry from '@sentry/node';
-// import cookieParser from 'cookie-parser';
-import * as cookie from 'cookie';
-import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
-// import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 
 import { logger } from './core/logger';
 import { httpLogger } from './core/logger/middleware';
@@ -36,42 +34,19 @@ process.on('unhandledRejection', (reason: unknown) => {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:3000',
-      'https://challenge-tracker.riabkov.com',
-    ],
-    credentials: true,
-  }),
-);
-
-// app.use(helmet({ crossOriginResourcePolicy: false }));
+app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(express.json({ limit: '10kb' }));
-// app.use(cookieParser());
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  logger.info(`[CookieParser] Parsing cookies: ${req.headers.cookie}`);
-
-  const cookies = cookie.parse(req.headers.cookie ?? '');
-
-  logger.info(`[CookieParser] Parsed cookies: ${JSON.stringify(cookies)}`);
-
-  req.cookies = cookies;
-
-  next();
-});
+app.use(cookieParser());
 
 app.use(httpLogger);
 
-app.use('/auth', AuthRouter);
+app.use('/api/auth', AuthRouter);
 
-app.use('/protected/user', authMiddleware, UserRoute);
+app.use('/api/protected/user', authMiddleware, UserRoute);
 
-app.use('/protected/challenge', authMiddleware, ChallengeRoute);
+app.use('/api/protected/challenge', authMiddleware, ChallengeRoute);
 
-app.get('/healthcheck', (req: Request, res: Response) => {
+app.get('/api/healthcheck', (req: Request, res: Response) => {
   return res.status(200).send('OK');
 });
 

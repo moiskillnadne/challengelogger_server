807bb8ba058c70bdd7833bd46d6247813e046075
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-14 18:26:17 +0200

    User validation added to /protected/user

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,16 +1,17 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
-// import { ErrorMessages } from '~/core/dictionary/error.messages';
-// import { UnauthorizedError } from '~/core/errors';
-import { logger } from '~/core/logger';
-// import { isAuthenticated } from '~/shared/user';
+import { ErrorMessages } from '~/core/dictionary/error.messages';
+import { UnauthorizedError } from '~/core/errors';
+import { isAuthenticated } from '~/shared/user';
 
 const route = express.Router();
 
-route.get('/', (req: Request, res: Response) => {
+route.get('/', (req: Request, res: Response, next: NextFunction) => {
   const user = req.user;
 
-  logger.info(`[GET /protected/user] User: ${JSON.stringify(user)}`);
+  if (!isAuthenticated(user)) {
+    return next(new UnauthorizedError(ErrorMessages.unauthorized));
+  }
 
   return res.status(200).json({
     type: 'USER_FETCHED',

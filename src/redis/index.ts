5e13fe40eb9d8d663982707342768f16096975b6
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-14 17:28:36 +0200

    Updated connection to redis

--- src/redis/index.ts
+++ src/redis/index.ts
@@ -2,7 +2,9 @@ import { createClient } from 'redis';
 
 import { logger } from '../core/logger';
 
-export const redis = createClient();
+export const redis = createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/${process.env.REDIS_DB}`,
+});
 
 redis.on('error', (err) => {
   logger.error('Redis error: ', JSON.stringify(err));

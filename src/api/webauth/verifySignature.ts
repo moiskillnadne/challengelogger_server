92cd891d17821ecf96a4bb6484fbabeb8a5f6710
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-18 14:09:59 +0200

    convert public key to pem

--- src/api/webauth/verifySignature.ts
+++ src/api/webauth/verifySignature.ts
@@ -1,3 +1,4 @@
+import cbor from 'cbor';
 import { createVerify } from 'crypto';
 
 /**
@@ -15,8 +16,39 @@ export function verifySignature(
 ): boolean {
   const verifier = createVerify('sha256');
 
+  const pem = coseToPem(publicKey);
+
   verifier.update(data);
   verifier.end();
 
-  return verifier.verify(publicKey, signature);
+  return verifier.verify(pem, signature);
+}
+
+/**
+ * Преобразование COSE-публичного ключа в PEM-формат
+ * @param publicKey - COSE-публичный ключ
+ * @returns Публичный ключ в PEM-формате
+ */
+function coseToPem(cosePublicKey: Buffer): string {
+  // Декодируем COSE-публичный ключ (используем cbor)
+  const publicKeyStruct = cbor.decode(cosePublicKey);
+
+  // Извлекаем ключи x и y из структуры
+  const x = publicKeyStruct.get(-2);
+  const y = publicKeyStruct.get(-3);
+
+  // Добавляем стандартный заголовок и объединяем x и y для создания ключа
+  const pubKey = Buffer.concat([Buffer.from([0x04]), x, y]);
+
+  // Формируем PEM-формат для публичного ключа
+  const pem = `
+-----BEGIN PUBLIC KEY-----
+${pubKey
+  .toString('base64')
+  .match(/.{1,64}/g)
+  .join('\n')}
+-----END PUBLIC KEY-----
+  `.trim();
+
+  return pem;
 }

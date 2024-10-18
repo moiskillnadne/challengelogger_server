43706dc9d807826fe7947166593cae65848ff95b
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-18 17:36:54 +0200

    Added RP constants

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './timeInMs';
 export * from './cookies';
 export * from './env';
+export * from './rp';
---
+++ src/core/constants/rp.ts
@@ -0,0 +1,5 @@
+export const rpName = 'Challenge Logger';
+
+export const rpID = 'devapp.challengelogger.com';
+
+export const origin = `https://${rpID}`;

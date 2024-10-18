a7bc1e8ac675454b6a816c2535487547bebe479e
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-18 17:37:39 +0200

    Created UserCredentialCrudService

---
+++ src/shared/UserCredential/UserCredential.crud.ts
@@ -0,0 +1,35 @@
+import { Model } from 'sequelize';
+
+import { UserCredential } from '~/database/models/UserCredential';
+
+interface CreateCredentialPayload {
+  credId: string;
+  publicKey: Uint8Array;
+  webauthnUserID: string;
+  counter: number;
+  deviceType: string;
+  backedUp: boolean;
+  transports?: string[];
+  userId: string;
+}
+
+export class UserCredentialCrudService {
+  static async getCredentialByUserId(
+    userId: string,
+  ): Promise<Model<typeof UserCredential>[] | null> {
+    return UserCredential.findAll({ where: { userId } });
+  }
+
+  static async saveCredential(payload: CreateCredentialPayload) {
+    return UserCredential.create({
+      credId: payload.credId,
+      publicKey: payload.publicKey,
+      webauthnUserID: payload.webauthnUserID,
+      counter: payload.counter,
+      deviceType: payload.deviceType,
+      backedUp: payload.backedUp,
+      transports: payload.transports,
+      userId: payload.userId,
+    });
+  }
+}

5d162141a6ca88797dc3f57f73d70b0f40b4b4a1
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-10-18 18:27:51 +0200

    Update rpID

--- src/core/constants/rp.ts
+++ src/core/constants/rp.ts
@@ -1,5 +1,5 @@
 export const rpName = 'Challenge Logger';
 
-export const rpID = 'devapp.challengelogger.com';
+export const rpID = 'devapp.challengelogger.ru';
 
 export const origin = `https://${rpID}`;

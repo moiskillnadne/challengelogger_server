20a2791ab68b6e8ca53a51b22ca0d96500c9cbd2
vmkhitaryanscn <120190555+vmkhitaryanscn@users.noreply.github.com>
2024-11-20 11:32:55 +0400

    Feat: get | delete device meta (#28)
    * Feat: get device meta by user id, delete device meta by id
    
    * Fix eslint errors
    
    ---------
    
    Co-authored-by: Viktor Riabkov <vitya.ryabkov@gmail.com>

--- src/api/userDevice/controller.ts
+++ src/api/userDevice/controller.ts
@@ -1,11 +1,20 @@
 import express, { NextFunction, Request, Response } from 'express';
 
-import { UserDeviceCrud } from '~/api/userDevice/device.crud';
+import {
+  UserDevice,
+  UserDeviceCrud,
+  UserDeviceResult,
+} from '~/api/userDevice/device.crud';
 import { CreateDeviceSchema } from '~/api/userDevice/validation.schema';
 import { ErrorMessages } from '~/core/dictionary/error.messages';
-import { UnauthorizedError, UnprocessableEntityError } from '~/core/errors';
+import {
+  NotFoundError,
+  UnauthorizedError,
+  UnprocessableEntityError,
+} from '~/core/errors';
 import { logger } from '~/core/logger';
 import { authMiddleware } from '~/core/middleware/auth';
+import { modelToPlain } from '~/core/utils';
 import { isAuthenticated } from '~/shared/user';
 
 const route = express.Router();
@@ -32,7 +41,7 @@ route.post(
 
       const deviceInfo = req.body;
 
-      await UserDeviceCrud.saveCredential({
+      await UserDeviceCrud.saveDevice({
         userId: user.id,
         fingerprint: deviceInfo.fingerprint,
         platform: deviceInfo.platform,
@@ -54,6 +63,80 @@ route.post(
     } catch (error: unknown) {
       logger.error(`Error verifying device-meta: ${error}`);
       return res.status(404).json({ isSuccess: false, error });
+    }
+  },
+);
+
+route.get(
+  '/',
+  authMiddleware,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!isAuthenticated(user)) {
+      return next(new UnauthorizedError(ErrorMessages.unauthorized));
+    }
+
+    logger.info(`[${req.traceId}] Get user devices started by: ${user.email}`);
+
+    const userDevicesEntities = await UserDeviceCrud.getDevicesByUserId(
+      user.id,
+    );
+
+    const userDevices = modelToPlain<Array<UserDevice>>(userDevicesEntities);
+
+    const userDevicesResult: UserDeviceResult[] = userDevices.map((device) => ({
+      id: device.id,
+      createdAt: device.createdAt,
+      updatedAt: device.updatedAt,
+      platform: device.platform,
+      platformVersion: device.platformVersion,
+      browser: device.browser,
+      browserVersion: device.browserVersion,
+      screenResolution: device.screenResolution,
+      colorDepth: device.colorDepth,
+      pixelDepth: device.pixelDepth,
+      webGLFingerprint: device.webGLFingerprint,
+      pixelRatio: device.pixelRatio,
+      maxTouchPoints: device.maxTouchPoints,
+      isTouchScreen: device.isTouchScreen,
+      userAgent: device.userAgent,
+      userId: device.userId,
+    }));
+
+    res.status(200).json(userDevicesResult);
+  },
+);
+
+route.delete(
+  '/:deviceId',
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!isAuthenticated(user)) {
+      return next(new UnauthorizedError(ErrorMessages.unauthorized));
+    }
+
+    const deviceId = req.params.deviceId;
+
+    try {
+      const deleteResult = await UserDeviceCrud.deleteOneByParams({
+        id: deviceId,
+        userId: user.id,
+      });
+
+      if (deleteResult === 0) {
+        throw new NotFoundError('Device not found');
+      }
+
+      return res.status(200).json({
+        type: 'DEVICE_DELETED',
+        statusCode: 200,
+        message: 'Device deleted successfully',
+        isSuccess: true,
+      });
+    } catch (error: unknown) {
+      return next(error);
     }
   },
 );

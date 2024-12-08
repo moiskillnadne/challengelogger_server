a9fe21c3f8ec34747cc4db449683410bf20c2664
Viktor Riabkov <vitya.ryabkov@gmail.com>
2024-12-08 15:51:10 +0700

    Update migration

---
+++ src/database/migrations/20241208081622-add-status-to-userChallenge.js
@@ -0,0 +1,46 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const { Op } = Sequelize;
+
+    await queryInterface.addColumn('userChallenge', 'status', {
+      type: Sequelize.TEXT,
+      allowNull: true,
+      unique: false,
+      defaultValue: null,
+    });
+
+    const currentDate = new Date();
+    const currentMonth = currentDate.getMonth();
+    const currentYear = currentDate.getFullYear();
+
+    const formattedCurrentMonth = ('0' + currentMonth).slice(-2);
+
+    await queryInterface.bulkUpdate(
+      'userChallenge',
+      { status: 'ACTIVE' },
+      Sequelize.and(
+        Sequelize.where(
+          Sequelize.fn('substr', Sequelize.col('startedAtDate'), 6, 2), // Extract month from string with format YYYY-MM-DD
+          formattedCurrentMonth,
+        ),
+        Sequelize.where(
+          Sequelize.fn('substr', Sequelize.col('startedAtDate'), 1, 4), // Extract year from string with format YYYY-MM-DD
+          currentYear.toString(),
+        ),
+      ),
+    );
+
+    await queryInterface.bulkUpdate(
+      'userChallenge',
+      { status: 'COMPLETED' },
+      { status: null },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('userChallenge', 'status');
+  },
+};
